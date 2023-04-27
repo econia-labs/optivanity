040bab32329da076fb07d84c53a568917bc3056b
qdrs <71022090+qdrs@users.noreply.github.com>
2023-04-27 11:32:54 +0900

    ECO-114 + ECO-113: use clap + crossbeam + anyhow (#1)
    * use clap + crossbeam + anyhow
    
    * Revise channel/parser modifications, update README
    
    * minor changes
    
    * Remove error note per input checking mod
    
    * Remove unused import
    
    * fix parser
    
    * print errors from thread
    
    * Update comments, README per changes/suggestions
    
    ---------
    
    Co-authored-by: alnoki <43892045+alnoki@users.noreply.github.com>

--- README.md
+++ README.md
@@ -5,10 +5,11 @@
 - [`optivanity`, brought to you by Econia Labs](#optivanity-brought-to-you-by-econia-labs)
   - [Quickstart](#quickstart)
     - [Setup](#setup)
-    - [Quad-core Ed25519 vanity address generation](#quad-core-ed25519-vanity-address-generation)
+    - [Ed25519 vanity address generation](#ed25519-vanity-address-generation)
     - [Octa-core multisig vanity address generation](#octa-core-multisig-vanity-address-generation)
   - [General](#general)
   - [Parallelism](#parallelism)
+  - [CPU load](#cpu-load)
 
 ## Quickstart
 
@@ -17,84 +18,93 @@
 ```zsh
 % git clone https://github.com/econia-labs/optivanity.git
 % cd optivanity
-% cargo build --release
+% cargo run --release -- --help
+
+Optivanity: hyper-parallelized vanity address generator for the Aptos blockchain, brought to you by Econia Labs
+
+Usage: optivanity [OPTIONS] --prefix <PREFIX>
+
+Options:
+  -p, --prefix <PREFIX>    Address prefix to match (no leading `0x`). Each additional character slows search by 16x
+  -m, --multisig           Use this flag if you want to search for multisig address(es)
+  -c, --count <COUNT>      Number of vanity accounts to generate [default: 1]
+  -t, --threads <THREADS>  Number of threads to use. Only specify if you want to use fewer cores than available [default: 10]
+  -h, --help               Print help
 ```
 
-### Quad-core Ed25519 vanity address generation
+### Ed25519 vanity address generation
 
 ```zsh
-# Generate standard account addresses starting with aaaaa, parallelized across 4 cores
-% cargo run --release -- aaaaa 4
-Standard account address: 0xaaaaab5d00e4ffe9ba3add22f0c62b592b18d6b0401b12f1d0732e180cd45781
-Private key:              0x00d8a9b865324baa161925221536f4ea1f6da9a04b88dd717a1f8109d80ef242
+# Generate a single standard account address starting with aaaaa, maximum parallelism
+% cargo run --release -- --prefix aaaaa
 
-Standard account address: 0xaaaaa4637eb8cacd4b8781dae490b219a5bd37ac0241babbe42af73131e8fb28
-Private key:              0x234baa20f4f1c29bb0d1d1d760d2a5059dc3eb7d9cd3ac5397fa717234691e18
+Starting search at 2023-04-26T12:06:11.583917-07:00
 
-Standard account address: 0xaaaaaef43df45095219c39e15762fa71c58d147dbeb999646552109bfda53ca7
-Private key:              0x8f5e2806c2819159a34e83e78f5967410974964f4e3434fb599689e0891a7b2d
+Standard account address: 0xaaaaa08d6dd99050567e11eb5ac338c8b7976a94d8dc07d2c346fc60e12a5d32
+Private key:              0x3912b341c3763f9193dab75df849dbe232ca1e0234dec690e78578e30ebc8e20
 
-^C
-# (Press Ctrl + c to stop the search)
+Elapsed time: 1.059834208s
 ```
 
 ### Octa-core multisig vanity address generation
 
 ```zsh
-# Generate multisig account addresses starting with bbbbbb, parallelized across 8 cores
-% cargo run --release -- bbbbbb 8 -m
+# Generate 3 multisig account addresses starting with bbbbbb, parallelized across 8 cores
+% cargo run --release -- --prefix bbbbbb --multisig --count 3 --threads 8
 
-Multisig account address: 0xbbbbbb8dfacadee4c12094e8477eb1b306e1be5ad6aa355a544953fc8aeeda82
-Standard account address: 0x9950b5fd7bfca1e42341e7363aced4400beefdf9c73658b2b346916c6485425b
-Private key:              0x331b54bbf7d21fb4064020f391ee81f76bdf21e89c130a91dae18486963e3d74
+Starting search at 2023-04-26T12:06:57.444782-07:00
 
-Multisig account address: 0xbbbbbb29f65eb8d01b0ce0653e0a2ef782f770c1398d5dbb5474746515be337a
-Standard account address: 0xa0f77fce095349d2b5a52cda01a831fce8135cbd403956254ca92cc067184a22
-Private key:              0x05bc61b9d36a7f984fd57337ac9c5fca784e639c88dd0e1bf65f72186446cb36
+Multisig account address: 0xbbbbbbf1061840cccc8542b98ace03b73a2ffc7df609fbaf99c546982a8b7dc8
+Standard account address: 0xfc05df546a70c2fafeb8b9b19637fc9b70c2300e1e6f570c6369798f4afd6c77
+Private key:              0xd220c6fb9df3836d8e1463435cb427ff270b3ce417769adaf5903ebd49560da4
 
-^C
-# (Press Ctrl + c to stop the search)
+Multisig account address: 0xbbbbbb4ab601a7c40b96384ef63a357609ad843056fbab6158bb396dd3193878
+Standard account address: 0x490f50f84013452a6660d99e8018acc1f20241da8321e9ddfb0d2b70c48742ef
+Private key:              0x7cd4bece605833845b00da1015e4615d48dffdf5714ce62980d305934f13df80
+
+Multisig account address: 0xbbbbbbfba754ccf12a05bf087051fe190928e41bafbeaa20ca94b6b066cfbed8
+Standard account address: 0x9f770fb89a85ce2916f127d3280d261234fc572d9949d7a9ffbe9b44251add20
+Private key:              0xa35f8534e883c1cf60b6c0d96863ac3eb54a9ce437708a1ac81762e268875312
+
+Elapsed time: 61.497349083s
 ```
 
 ## General
 
 `optivanity` provides vanity address generation functionality similar to that of the `aptos` CLI, but with assorted performance optimizations:
 
-- Thread count positional argument for configurable execution parallelism
+- Thread count argument for configurable execution parallelism (defaults to maximum possible parallelism)
 - Byte-wise search, instead of expensive string-wise search like in the `aptos` CLI
 - Build enhancements including [linker-time optimization](https://doc.rust-lang.org/cargo/reference/profiles.html#lto) and [code generation unit](https://doc.rust-lang.org/cargo/reference/profiles.html#codegen-units) minimization
 - Minimal crate includes for reduced compile times compared with `aptos` CLI
 
-`optivanity` runs in an infinite loop, generating vanity prefixes until the user stops the search by pressing `Ctrl + c`.
-Input checking is minimal, so use the exact syntax from the [quickstart](#quickstart):
-
-```bash
-# Optional '-m' flag indicates multisig vanity prefix
-% cargo run --release -- <PREFIX_WITHOUT_LEADING_0x> <THREAD_COUNT> [-m]
-```
-
-Don't forget to use `cargo`'s [`--release` flag](https://doc.rust-lang.org/cargo/reference/profiles.html#release) for maximal build performance, and note that each additional hex character will on average slow down the search by a factor of sixteen (an `aaaaaa` search will on average take 256 times as long as an `aaaa` search for a given thread count).
+Don't forget to use `cargo`'s [`--release` flag](https://doc.rust-lang.org/cargo/reference/profiles.html#release) for maximal build performance!
 
 ## Parallelism
 
-The thread count positional argument controls how many independent search threads will be initiated during execution.
+The optional thread count argument controls how many independent search threads will be initiated during execution, and defaults to the maximum amount possible on your machine.
 Two search threads running in parallel, for example, will on average take $\frac{1}{2}$ as long to generate an address compared with a single search thread, which is what the `aptos` CLI uses.
 Three threads will on average take $\frac{1}{3}$ as long, four will on average take $\frac{1}{4}$ as long, and so on, **until the thread count equals the number of available cores**:
-if your machine only has four available cores, you will not generate performance increases for a five-thread search because you can only run four threads in parallel.
+if your machine only has four available cores, you will not see performance increases for a five-thread search because you can only run four threads in parallel.
+
+In other words, *only* specify thread count if you want to slow down the search for machine longevity.
+
+## CPU load
 
 The algorithms in `optivanity` were developed on a 2021 MacBook Pro with a ten-core [Apple M1 Max chip](https://en.wikipedia.org/wiki/Apple_M1#M1_Pro_and_M1_Max), where the optimal thread count for search speed is ten.
 This is probably not the optimal thread count for machine longevity, however, because a ten-thread search results in the fan running full blast to prevent overheating.
 Running with only six threads does not result in the fan noticeably turning on and is sufficient, for example, to generate an address with an eight-character vanity prefix overnight.
 
-`optivanity` relies on a main thread with a quasi-infinite delay to prevent the closure of search threads.
-Hence in the "Activity Monitor" app for the above machine, `cargo run --release -- aaaaa 6` search results in the following:
+`optivanity` relies on a main watchdog thread that closes search threads once enough addresses have been generated.
+Hence for the "Activity Monitor" app on the above machine, the following command results in the following readout:
+
+```zsh
+cargo run --release -- --prefix aaaaa --threads 6 --count 100
+```
 
 | Process Name | % CPU | Threads |
 | ------------ | ----- | ------- |
 | `optivanity` | 600   | 7       |
 
 Here, six cores are each running a search thread at ~100% capacity, with a seventh non-search thread consuming almost no load.
-Hence without other major processes running, this results in a user CPU load of slightly above 60%.
-
-Again, the optimal thread count is architecture specific, and your machine will not be able to accommodate parallelism in excess of the number of available cores:
-running a twenty-thread search on the above machine, for example, results in a performance *decrease* over a ten-thread search because the system scheduler has to constantly pause and restart threads.
+Hence without other major processes running, this results in a user CPU load of about 60%.
--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,170 @@
+use anyhow::Result;
 use aptos_crypto::ed25519::{Ed25519PrivateKey, Ed25519PublicKey};
 use aptos_keygen::KeyGen;
 use aptos_types::{
     account_address::{create_multisig_account_address, AccountAddress},
     transaction::authenticator::AuthenticationKey,
 };
-use std::env;
-use std::thread;
-use std::time::Duration;
+use chrono::prelude::{DateTime, Local};
+use clap::Parser;
+use crossbeam::channel;
+use std::thread::{self, available_parallelism};
+use std::time::{Instant, SystemTime};
 
-const PREFIX_ARG: usize = 1;
-const N_THREADS_ARG: usize = 2;
-const MULTISIG_ARG: usize = 3;
-const MULTISIG_FLAG: &str = "-m";
-const SEQUENCE_NUMBER: u64 = 0;
+/// Multisig account generation is assumed to take place in first transaction of standard account.
+const SEQUENCE_NUMBER_MULTISIG: u64 = 0;
+
+/// Optivanity: hyper-parallelized vanity address generator for the Aptos blockchain, brought to you
+/// by Econia Labs
+#[derive(Parser, Debug)]
+struct CliArgs {
+    /// Address prefix to match (no leading `0x`). Each additional character slows search by 16x.
+    #[arg(short, long)]
+    prefix: String,
+    /// Use this flag if you want to search for multisig address(es)
+    #[arg(short, long)]
+    multisig: bool,
+    /// Number of vanity accounts to generate
+    #[arg(short, long, default_value_t = 1)]
+    count: u64,
+    /// Number of threads to use. Only specify if you want to use fewer cores than available.
+    #[arg(short, long, default_value_t = available_parallelism().unwrap().get())]
+    threads: usize,
+}
 
 /// Derive authentication key bytes vector from a reference to a private key.
 fn auth_key_bytes_vec(private_key_ref: &Ed25519PrivateKey) -> Vec<u8> {
     AuthenticationKey::ed25519(&Ed25519PublicKey::from(private_key_ref)).to_vec()
 }
 
-fn main() {
-    // Get command arguments.
-    let args: Vec<_> = env::args().collect();
-    let mut check_str = args[PREFIX_ARG].clone(); // Get prefix string for format check.
-    let is_odd_length = check_str.len() % 2 != 0; // Check if string has odd character count.
-    if is_odd_length {
-        // Append a 0 to check string if an odd number of hex characters.
-        check_str.push('0')
-    };
-    if hex::decode(check_str).is_err() {
-        println!("Vanity prefix must contain valid hex characters (0123456789abcdef).");
-        return; // Exit if vanity prefix isn't specified in valid hex characters.
+/// Parse command line arguments, verifying hex characters and specified thread count.
+fn parse_args() -> Result<CliArgs> {
+    let args = CliArgs::parse();
+
+    // Verify prefix has valid hex characters, appending a 0 as needed for an odd character count.
+    if has_odd_character_count(&args.prefix) {
+        hex::decode(format!("{}0", &args.prefix))?;
+    } else {
+        hex::decode(&args.prefix)?;
     }
-    // Check if should search for multisig vanity address.
-    let multisig = args.len() > MULTISIG_ARG && args[MULTISIG_ARG].as_str() == MULTISIG_FLAG;
-    // Spawn parallel search threads.
-    for _ in 0..args[N_THREADS_ARG].parse::<u64>().unwrap() {
-        thread::spawn(move || {
-            // Get random key generator.
-            let mut key_generator = KeyGen::from_os_rng();
-            // Get vanity prefix as string.
-            let prefix_str = env::args().collect::<Vec<_>>()[PREFIX_ARG].clone();
-            // Clone prefix for getting bytes to check against.
-            let mut check_str = prefix_str.clone();
-            // Pop last character if an odd length.
-            if is_odd_length {
-                check_str.pop();
+
+    Ok(args)
+}
+
+/// Return `true` if string slice reference indicates a string with an odd number of characters.
+fn has_odd_character_count(string_ref: &str) -> bool {
+    string_ref.len() % 2 == 1
+}
+
+/// Generate a private key corresponding to a vanity prefix, while search is ongoing.
+///
+/// Once a match is found, a match message is transmitted to the main thread. Once the main thread
+/// has received sufficient match transactions, it broadcasts an exit transaction to search threads.
+///
+/// # Arguments
+///
+/// * `prefix` - The vanity prefix to search against
+/// * `multisig` - If `true` search for a multisig address
+/// * `match_tx` - Transmit channel for match message sent to main thread when a match is found
+/// * `exit_rx` - Receive channel for exit message broadcast from main thread when search is done
+fn generate_key(
+    prefix: String,
+    multisig: bool,
+    match_tx: channel::Sender<()>,
+    exit_rx: channel::Receiver<()>,
+) -> Result<()> {
+    // Clone prefix for getting bytes to check against.
+    let mut check_str = prefix.clone();
+    // Pop last character of check string if an odd length.
+    if has_odd_character_count(&check_str) {
+        check_str.pop();
+    };
+
+    // Convert resulting string to bytes for checking against. For odd-length prefixes this approach
+    // truncates the final character, which must be checked via string compare after a bytes match.
+    // This is done for performance (to minimize string operations during the loop).
+    let prefix_bytes = hex::decode(check_str)?;
+
+    // Randomly generate private keys in a loop and check match against prefix bytes.
+    let mut key_generator = KeyGen::from_os_rng();
+    loop {
+        // Exit search loop if exit message has been broadcast.
+        if exit_rx.try_recv().is_ok() {
+            return Ok(());
+        }
+        // Generate a private key and from it, bytes to compare against prefix bytes.
+        let private_key = key_generator.generate_ed25519_private_key();
+        let account_address_bytes = auth_key_bytes_vec(&private_key);
+        let search_bytes = if multisig {
+            create_multisig_account_address(
+                AccountAddress::from_bytes(account_address_bytes)?,
+                SEQUENCE_NUMBER_MULTISIG,
+            )
+            .to_vec()
+        } else {
+            account_address_bytes
+        };
+
+        // If a match found, print diagnostics then keep searching.
+        if search_bytes.starts_with(&prefix_bytes) {
+            let search_str = hex::encode(search_bytes);
+            // Verify full string match only if prefix has an odd character count.
+            if has_odd_character_count(&prefix) && !search_str.starts_with(&prefix) {
+                continue;
+            }
+            // Get standard account address. If multisig:
+            let account_address_hex = if multisig {
+                println!("Multisig account address: 0x{}", search_str);
+                // Re-derive account address here rather than cloning during search.
+                hex::encode(auth_key_bytes_vec(&private_key))
+            } else {
+                // If not multisig, standard account address is from search.
+                search_str
             };
-            // Convert resulting prefix to bytes for checking against. For odd-length prefixes this
-            // approach truncates the final character, which must be checked via string compare
-            // after a bytes match. This is done for performance (to minimize string operations).
-            let prefix_bytes = hex::decode(check_str).unwrap();
-            // Generate private keys, reporting for each match against vanity prefix.
-            loop {
-                let private_key = key_generator.generate_ed25519_private_key();
-                // Assume account address is identical to authentication key (no rotation).
-                let account_address_bytes = auth_key_bytes_vec(&private_key);
-                // Get bytes to check against: if multisig, derive multisig account address.
-                let search_bytes = if multisig {
-                    // Note that for performance this moves (rather than clones) address bytes, such
-                    // that account address must be re-derived from private key if a match is found.
-                    create_multisig_account_address(
-                        AccountAddress::from_bytes(account_address_bytes).unwrap(),
-                        SEQUENCE_NUMBER,
-                    )
-                    .to_vec()
-                } else {
-                    account_address_bytes // If standard account, check against account address.
-                };
-                // If a match found, print diagnostics then keep searching.
-                if search_bytes.starts_with(&prefix_bytes) {
-                    // Get search bytes as a string.
-                    let search_str = hex::encode(search_bytes);
-                    // Verify full string match if prefix is an odd number of characters.
-                    if is_odd_length && !search_str.starts_with(&prefix_str) {
-                        continue;
-                    }
-                    // Get standard account address. If multisig:
-                    let account_address_hex = if multisig {
-                        println!("Multisig account address: 0x{}", search_str);
-                        // Re-derive account address bytes here rather than cloning during search.
-                        hex::encode(auth_key_bytes_vec(&private_key))
-                    } else {
-                        search_str // If not multisig, standard account address is from search.
-                    };
-                    println!("Standard account address: 0x{}", account_address_hex);
-                    println!(
-                        "Private key:              0x{}\n",
-                        hex::encode(private_key.to_bytes())
-                    );
-                }
+            println!("Standard account address: 0x{}", account_address_hex);
+            println!(
+                "Private key:              0x{}\n",
+                hex::encode(private_key.to_bytes())
+            );
+            // Transmit match message back to main thread.
+            match_tx.send(())?;
+        }
+    }
+}
+
+/// Parses arguments, starts a timer, then spawns parallel search threads. Once search threads have
+/// transmitted back enough match messages, broadcasts an exit transaction and prints elapsed time.
+fn main() -> Result<()> {
+    let start_time = Instant::now();
+    let args = parse_args()?;
+    println!(
+        "\nStarting search at {:#?}\n",
+        DateTime::<Local>::from(SystemTime::now())
+    );
+
+    // Initialize message channels for match and exit messages.
+    let (match_tx, match_rx) = channel::unbounded::<()>();
+    let (exit_tx, exit_rx) = channel::bounded::<()>(1);
+
+    // Spawn parallel search threads.
+    for _ in 0..args.threads {
+        // Locally clone arguments not implementing copy trait so they can be moved into closure.
+        let match_tx = match_tx.clone();
+        let exit_rx = exit_rx.clone();
+        let prefix = args.prefix.clone();
+        thread::spawn(move || {
+            if let Err(e) = generate_key(prefix, args.multisig, match_tx, exit_rx) {
+                println!("Error: {}, in thread: {:?}", e, thread::current().id());
             }
         });
     }
-    // Run infinite loop to prevent main thread from returning and closing search threads.
-    loop {
-        thread::sleep(Duration::from_secs(u64::MAX)); // Sleep to preserve resources.
+
+    // Stop search after the desired number of addresses have been generated.
+    for _ in 0..args.count {
+        match_rx.recv()?;
     }
+    exit_tx.send(())?;
+
+    println!("Elapsed time: {:#?}\n", start_time.elapsed());
+    Ok(())
 }
